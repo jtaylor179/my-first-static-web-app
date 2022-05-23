9a22bba0d7ca2ecf53c78c73be087ba087c01848
Jeff Taylor <jtaylor179@hotmail.com>
2022-05-22 20:33:27 -0400

    upgrade vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>{{ message }}</div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
-  data() {
-    return {
-      message: ""
-    };
+  name: 'App',
+  components: {
+    HelloWorld
   },
   async mounted() {
     const { text } = await (await fetch("/api/getData")).json();
+    console.log(text);
     this.message = text;
   }
-};
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
